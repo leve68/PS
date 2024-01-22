b8bf285140eaa6db631df2a7b248e5c0e059f721
leve68 <128668023+leve68@users.noreply.github.com>
2024-01-22 18:41:51 +0900

    [Silver III] Title: N과 M (2), Time: 124 ms, Memory: 9332 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.js
@@ -0,0 +1,35 @@
+const input = require("fs")
+  .readFileSync("/dev/stdin")
+  .toString()
+  .trim()
+  .split(" ")
+  .map((x) => Number(x));
+
+const n = input[0];
+const m = input[1];
+
+//1-n 사이의 수 m개 선택
+
+let visited = Array(n).fill(false);
+let arr = [];
+let answer = [];
+DFS(n, m, 0, 0);
+console.log(answer.join("\n"));
+
+function DFS(n, m, count, last) {
+  if (count === m) {
+    answer.push(arr.join(" "));
+    return;
+  }
+
+  for (let i = last; i < n; i++) {
+    if (!visited[i]) {
+      visited[i] = true;
+      arr[count] = i + 1;
+
+      DFS(n, m, count + 1, i);
+
+      visited[i] = false;
+    }
+  }
+}
