4857b15dd7cabfe9288b4e7f05b2045c3c0ed49a
SeungYeon Han <hanstom000608@gmail.com>
2025-03-14 08:23:40 +0900

    [Gold III] Title: 나만 안되는 연애, Time: 212 ms, Memory: 20908 KB -BaekjoonHub

--- 백준/Gold/14621. 나만 안되는 연애/나만 안되는 연애.java
+++ 백준/Gold/14621. 나만 안되는 연애/나만 안되는 연애.java
@@ -1,81 +1,75 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
-public class Main {
+class Main
+{
+    public static void main(String args[]) throws Exception
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        char[] schools = new char[N+1];
+        boolean[] visited = new boolean[N+1];
+        for(int i = 1 ; i <= N ; i++){
+            schools[i] = st.nextToken().charAt(0);
+        }
+        ArrayList<int[]>[] edge = new ArrayList[N+1];
+        for(int i = 1; i <= N; i++) {
+            edge[i] = new ArrayList<>();
+        }
+        for(int i = 0 ; i < M ; i++){
+            st = new StringTokenizer(br.readLine());
+            int v1 = Integer.parseInt(st.nextToken());
+            int v2 = Integer.parseInt(st.nextToken());
+            int d = Integer.parseInt(st.nextToken());
+            edge[v1].add(new int[] {v2, d});
+            edge[v2].add(new int[] {v1, d});
+        }
 
-	static int n, m, cnt;
-	static int[] col, parent;
+        int startV = 1;
+        int mstSum = 0;
+        visited[startV] = true;
+        PriorityQueue<int[]> pq = new PriorityQueue<int[]>((a,b) -> a[1] - b[1]);
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		n = Integer.parseInt(st.nextToken());
-		m = Integer.parseInt(st.nextToken());
+        for(int i = 0 ; i < edge[startV].size() ; i++){
+            int nv = edge[startV].get(i)[0];
+            int nd = edge[startV].get(i)[1];
+            if(schools[nv] != schools[startV]) pq.add(new int[] {nv, nd});
+        }
 
-		col = new int[n + 1];
-		st = new StringTokenizer(br.readLine());
-		for (int i = 1; i < n + 1; i++) {
-			char c = st.nextToken().charAt(0);
-			if (c == 'W')
-				col[i] = 1;
-		}
+        while(!pq.isEmpty()){
+            int[] c = pq.poll();
+            int cv = c[0];
+            int cd = c[1];
 
-		parent = new int[n + 1];
-		for (int i = 1; i < n + 1; i++) {
-			parent[i] = i;
-		}
+            if(visited[cv]) continue;
 
-		cnt = 0;
+            visited[cv] = true;
+            mstSum += cd;
 
-		ArrayList<int[]> list = new ArrayList<>();
-		for (int i = 0; i < m; i++) {
-			st = new StringTokenizer(br.readLine());
-			int a = Integer.parseInt(st.nextToken());
-			int b = Integer.parseInt(st.nextToken());
-			int c = Integer.parseInt(st.nextToken());
-			if (col[a] == col[b])
-				continue;
-			list.add(new int[] { a, b, c });
-		}
+            for(int i = 0 ; i < edge[cv].size() ; i++){
+                int nv = edge[cv].get(i)[0];
+                int nd = edge[cv].get(i)[1];
+                if(!visited[nv] && schools[nv] != schools[cv]) {
+                    pq.add(new int[] {nv, nd});
+                }
+            }
+        }
 
-		list.sort((o1, o2) -> o1[2] - o2[2]);
+        boolean allConnected = true;
+        for(int i = 1; i <= N; i++) {
+            if(!visited[i]) {
+                allConnected = false;
+                break;
+            }
+        }
 
-		for (int i = 0; i < list.size(); i++) {
-			int[] cur = list.get(i);
-			int a = cur[0];
-			int b = cur[1];
-			int c = cur[2];
-
-			if (find(a) != find(b)) {
-				cnt += c;
-				union(a, b);
-			}
-		}
-
-		for (int i = 1; i < n + 1; i++) {
-			if (find(i) != 1) {
-				System.out.println("-1");
-				return;
-			}
-		}
-
-		System.out.println(cnt);
-	}
-
-	private static void union(int a, int b) {
-		int a1 = find(a);
-		int b1 = find(b);
-
-		if (a1 > b1)
-			parent[a1] = b1;
-		else
-			parent[b1] = a1;
-	}
-
-	private static int find(int a) {
-		if (a == parent[a])
-			return a;
-		return parent[a] = find(parent[a]);
-	}
-
+        if(allConnected) {
+            System.out.println(mstSum);
+        } else {
+            System.out.println(-1);
+        }
+    }
 }
