591b98c1426f9cfbfd0d10ba9fdf7c5c76f6e910
SeungYeon Han <hanstom000608@gmail.com>
2025-07-04 16:18:24 +0900

    [Gold IV] Title: 음식 평론가, Time: 100 ms, Memory: 14108 KB -BaekjoonHub

---
+++ 백준/Gold/1188. 음식 평론가/음식 평론가.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        System.out.println(m - gcd(n, m));
+    }
+
+    private static int gcd(int a, int b) {
+        if (b == 0) {
+            return a;
+        }
+        return gcd(b, a % b);
+    }
+}
